5f9b4c8cae19874ce207bf982336cb47b26b7506
andriisukhoi <andrii.sukhoi@gmail.com>
2025-01-22 18:22:59 +0100

    add instagram link in navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,9 @@
 
 
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <link
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+        rel="stylesheet">
 </head>
 <body>
 <!-- Navbar -->
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,41 +1,50 @@
 @php use Mcamara\LaravelLocalization\Facades\LaravelLocalization; @endphp
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <!-- Logo -->
-        <a class="navbar-brand" href="{{ route('visuals') }}">
-            <img src="{{ asset('images/logo.png') }}" alt="Sala113" height="40">
-        </a>
+        <!-- Logo i menu -->
+        <div class="d-flex align-items-center">
+            <!-- Logo -->
+            <a class="navbar-brand me-3" href="{{ route('visuals') }}">
+                <img src="{{ asset('images/logo.png') }}" alt="Sala113" height="40">
+            </a>
+            <!-- Menu -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('visuals') }}">Visuals</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('about') }}">About</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="othersDropdown" role="button"
+                           data-bs-toggle="dropdown" aria-expanded="false">
+                            Others
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="othersDropdown">
+                            <li><a class="dropdown-item" href="{{ route('equipment') }}">Sprzęt</a></li>
+                            <li><a class="dropdown-item" href="{{ route('pricing') }}">Cennik</a></li>
+                            <li><a class="dropdown-item" href="{{ route('rules') }}">Regulamin</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
 
-        <!-- Toggle for mobile -->
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+        <!-- Ikona Instagrama (po środku) -->
+        <div class="text-center d-none d-lg-block">
+            <a href="https://www.instagram.com/sala113_lublin/" target="_blank" class="nav-link text-dark">
+                <i class="fab fa-instagram fa-lg"></i>
+            </a>
+        </div>
 
-        <!-- Links -->
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav me-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('visuals') }}">Visuals</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('about') }}">About</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="othersDropdown" role="button"
-                       data-bs-toggle="dropdown" aria-expanded="false">
-                        Others
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="othersDropdown">
-                        <li><a class="dropdown-item" href="{{ route('equipment') }}">Sprzęt</a></li>
-                        <li><a class="dropdown-item" href="{{ route('pricing') }}">Cennik</a></li>
-                        <li><a class="dropdown-item" href="{{ route('rules') }}">Regulamin</a></li>
-                    </ul>
-                </li>
-            </ul>
-            <!-- Language Selector -->
+        <!-- Przełącznik języków (po prawej) -->
+        <div class="d-flex">
             <ul class="navbar-nav">
-
                 @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
                     <li class="nav-item">
                         <a rel="alternate" hreflang="{{ $localeCode }}"
