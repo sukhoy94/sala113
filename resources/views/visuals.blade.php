c8caaa15a2c40943d9198ec0534ca33e9a1cbbad
andriisukhoi <andrii.sukhoi@gmail.com>
2025-04-13 06:24:40 +0200

    Move images on homepage to home folder

--- resources/views/visuals.blade.php
+++ resources/views/visuals.blade.php
@@ -6,35 +6,35 @@
     <div class="container my-5">
         <div class="row gx-4">
             <div class="col-md-6 mb-4" data-aos="fade-up">
-                <img src="{{ asset('images/photo7.jpg') }}" alt="Photo 7" class="img-fluid rounded">
+                <img src="{{ asset('images/home/photo7.jpg') }}" alt="Photo 7" class="img-fluid rounded">
             </div>
 
             <div class="col-md-6 mb-4" data-aos="fade-up">
-                <img src="{{ asset('images/photo2.jpg') }}" alt="Photo 2" class="img-fluid rounded">
+                <img src="{{ asset('images/home/photo2.jpg') }}" alt="Photo 2" class="img-fluid rounded">
             </div>
 
             <div class="col-md-6 mb-4" data-aos="fade-up">
-                <img src="{{ asset('images/photo3.jpg') }}" alt="Photo 3" class="img-fluid rounded">
+                <img src="{{ asset('images/home/photo3.jpg') }}" alt="Photo 3" class="img-fluid rounded">
             </div>
 
             <div class="col-md-6 mb-4" data-aos="fade-up">
-                <img src="{{ asset('images/photo4.jpg') }}" alt="Photo 4" class="img-fluid rounded">
+                <img src="{{ asset('images/home/photo4.jpg') }}" alt="Photo 4" class="img-fluid rounded">
             </div>
 
             <div class="col-md-6 mb-4" data-aos="fade-up">
-                <img src="{{ asset('images/photo5.jpg') }}" alt="Photo 5" class="img-fluid rounded">
+                <img src="{{ asset('images/home/photo5.jpg') }}" alt="Photo 5" class="img-fluid rounded">
             </div>
 
             <div class="col-md-6 mb-4" data-aos="fade-up">
-                <img src="{{ asset('images/photo6.jpg') }}" alt="Photo 6" class="img-fluid rounded">
+                <img src="{{ asset('images/home/photo6.jpg') }}" alt="Photo 6" class="img-fluid rounded">
             </div>
 
 
             <div class="col-md-6 mb-4" data-aos="fade-up">
-                <img src="{{ asset('images/photo1.jpg') }}" alt="Photo 1" class="img-fluid rounded">
+                <img src="{{ asset('images/home/photo1.jpg') }}" alt="Photo 1" class="img-fluid rounded">
             </div>
             <div class="col-md-6 mb-4" data-aos="fade-up">
-                <img src="{{ asset('images/photo8.jpg') }}" alt="Photo 8" class="img-fluid rounded">
+                <img src="{{ asset('images/home/photo8.jpg') }}" alt="Photo 8" class="img-fluid rounded">
             </div>
         </div>
     </div>
