a495800066804b55c85f72e1a2858b7cd20de52d
andriisukhoi <andrii.sukhoi@gmail.com>
2025-04-15 06:04:27 +0200

    Change order of photos on easter page + change text on easter page

--- resources/views/easter.blade.php
+++ resources/views/easter.blade.php
@@ -4,7 +4,6 @@
 
 @section('content')
     <div class="container my-5">
-        <!-- Sekcja z tekstem wprowadzającym -->
         <div class="row mb-5" data-aos="fade-up">
             <div class="col-12">
                 <h2 class="mb-4">{{ __('easter_title') }}</h2>
@@ -17,16 +16,7 @@
             </div>
         </div>
 
-        <!-- Galeria zdjęć w układzie 2 kolumnowym -->
         <div class="row gx-4">
-            <div class="col-md-6 mb-4" data-aos="fade-up">
-                <img src="{{ asset('images/easter/1.jpg') }}" alt="Easter 1" class="img-fluid rounded">
-            </div>
-
-            <div class="col-md-6 mb-4" data-aos="fade-up">
-                <img src="{{ asset('images/easter/2.jpg') }}" alt="Easter 2" class="img-fluid rounded">
-            </div>
-
             <div class="col-md-6 mb-4" data-aos="fade-up">
                 <img src="{{ asset('images/easter/3.jpg') }}" alt="Easter 3" class="img-fluid rounded">
             </div>
@@ -52,15 +42,11 @@
             </div>
 
             <div class="col-md-6 mb-4" data-aos="fade-up">
-                <img src="{{ asset('images/easter/9.jpg') }}" alt="Easter 9" class="img-fluid rounded">
+                <img src="{{ asset('images/easter/1.jpg') }}" alt="Easter 1" class="img-fluid rounded">
             </div>
 
             <div class="col-md-6 mb-4" data-aos="fade-up">
-                <img src="{{ asset('images/easter/10.jpg') }}" alt="Easter 10" class="img-fluid rounded">
-            </div>
-
-            <div class="col-12 mb-4" data-aos="fade-up">
-                <img src="{{ asset('images/easter/11.jpg') }}" alt="Easter 11" class="img-fluid rounded">
+                <img src="{{ asset('images/easter/2.jpg') }}" alt="Easter 2" class="img-fluid rounded">
             </div>
         </div>
     </div>
